adab72ddbc9cf2494cdf084d43c3400143933a65
afman42 <afifarman50@gmail.com>
2023-03-24 14:33:56 +0700

    change endpoint route folder sesi 2

--- sesi_2/routers/bookRouters.go
+++ sesi_2/routers/bookRouters.go
@@ -2,15 +2,16 @@ package routers
 
 import (
 	"challenge_sesi_2_api/controllers"
+
 	"github.com/gin-gonic/gin"
 )
 
 func StartServer() *gin.Engine {
 	router := gin.Default()
 
-	router.GET("/books", controllers.AllBooks)
+	router.GET("/book", controllers.AllBooks)
 	router.GET("/book/:bookID", controllers.GetBook)
-	router.POST("/book/add", controllers.CreateBook)
+	router.POST("/book", controllers.CreateBook)
 	router.PUT("/book/:bookID", controllers.UpdateBook)
 	router.DELETE("/book/:bookID", controllers.DeleteBook)
 
